d5241bf414e11dac940f924423d0f02d64e268c6
nathluu <luuhoangtanbis@gmail.com>
2024-05-28 20:16:24 +0700

    Add aisearch

--- local.tf
+++ local.tf
@@ -9,4 +9,20 @@ locals {
         private-cluster-enabled = false
         admin_group_object_ids  = ["a5d5db85-021c-436f-83b9-9e2b9070a0df"]
     }
+    aisearch = {
+        name                                = "natcom0aiserach01"
+        resource-group-name                 = "natcom-aisearch-rg"
+        location                            = "southeastasia"
+        vnet-name                           = "natcom-aisearch-vnet"
+        subnet-name                         = "natcom-aisearch-snet"
+        sku                                 = "basic"
+        local_authentication_enabled        = true
+        public_network_access_enabled       = true
+        allowed_ips                         = []
+        partition_count                     = 1
+        replica_count                       = 1
+        semantic_search_sku                 = "free"
+        private-endpoint-enabled            = false
+        admin_group_object_ids              = ["a5d5db85-021c-436f-83b9-9e2b9070a0df"]
+    }
 }
