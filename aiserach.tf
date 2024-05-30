8bd128b5ad0dfcb5bb4bfca9f4809d522be51fed
nathluu <luuhoangtanbis@gmail.com>
2024-05-30 08:37:54 +0700

    Remove PE for aisearch

--- aiserach.tf
+++ aiserach.tf
@@ -3,20 +3,20 @@ resource "azurerm_resource_group" "rg" {
     location = local.aisearch.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-    name                = local.aisearch.vnet-name
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    address_space       = ["10.0.0.0/16"]
-}
+# resource "azurerm_virtual_network" "vnet" {
+#     name                = local.aisearch.vnet-name
+#     location            = azurerm_resource_group.rg.location
+#     resource_group_name = azurerm_resource_group.rg.name
+#     address_space       = ["10.0.0.0/16"]
+# }
 
-resource "azurerm_subnet" "subnet" {
-    name                 = local.aisearch.subnet-name
-    resource_group_name  = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes     = ["10.0.1.0/24"]
-    service_endpoints    = ["Microsoft.ContainerRegistry"]
-}
+# resource "azurerm_subnet" "subnet" {
+#     name                 = local.aisearch.subnet-name
+#     resource_group_name  = azurerm_resource_group.rg.name
+#     virtual_network_name = azurerm_virtual_network.vnet.name
+#     address_prefixes     = ["10.0.1.0/24"]
+#     service_endpoints    = ["Microsoft.ContainerRegistry"]
+# }
 
 
 resource "azurerm_search_service" "search_service" {
@@ -37,16 +37,16 @@ resource "azurerm_search_service" "search_service" {
     }
 }
 
-resource "azurerm_private_endpoint" "pe" {
-  name                = "${local.aisearch.name}-pe"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.subnet.id
+# resource "azurerm_private_endpoint" "pe" {
+#   name                = "${local.aisearch.name}-pe"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = azurerm_subnet.subnet.id
 
-  private_service_connection {
-    name                           = "${local.aisearch.name}-pe-conn"
-    is_manual_connection           = false
-    private_connection_resource_id = azurerm_search_service.search_service.id
-    subresource_names              = ["searchservice"]
-  }
-}
+#   private_service_connection {
+#     name                           = "${local.aisearch.name}-pe-conn"
+#     is_manual_connection           = false
+#     private_connection_resource_id = azurerm_search_service.search_service.id
+#     subresource_names              = ["searchservice"]
+#   }
+# }
